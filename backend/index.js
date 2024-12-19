d6e7ab8bcca81f13e4982f432d257ff59e81078a
iujjwalrai <imujjwal.rai@gmail.com>
2024-12-19 19:13:37 +0530

    Fixed CORS issue

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000', // Your frontend's origin
+    origin: 'https://asct-frontend.onrender.com/', // Your frontend's origin
     credentials: true,               // Allows credentials (cookies)
   }));
   
